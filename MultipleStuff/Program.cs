c262161a3cc50940a36dd0f5e23e0cf98b7f5def
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-06-02 19:10:22 +0100

    Update SuperList (Program)

--- MultipleStuff/Program.cs
+++ MultipleStuff/Program.cs
@@ -6,7 +6,22 @@ namespace MultipleStuff
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            SuperList lista = new SuperList{5.5, 10.2, 1.8, 25.4, 12.0};
+
+            Console.WriteLine("Testing MinMax methods:");
+            
+            // MinMax1
+            lista.GetMinMax1(out double min1, out double max1);
+            Console.WriteLine($"MinMax1 - Min: {min1}, Max: {max1}");
+            // MinMax2
+            SuperList.MinMaxResult minMax2 = lista.GetMinMax2();
+            Console.WriteLine($"MinMax2 - Min: {minMax2.Min}, Max: {minMax2.Max}");
+            // MinMax3
+            Tuple<double, double> minMax3 = lista.GetMinMax3();
+            Console.WriteLine($"MinMax3 - Min: {minMax3.Item1}, Max: {minMax3.Item2}");
+            // MinMax4
+            (double min4, double max4) = lista.GetMinMax4();
+            Console.WriteLine($"MinMax4 - Min: {min4}, Max: {max4}");
         }
     }
 }
