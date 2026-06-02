7238cc296f1445ac5f56e8a85ced60001a6fa0d1
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-06-02 18:09:24 +0100

    Add MultipleStuff

---
+++ MultipleStuff/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultipleStuff
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
