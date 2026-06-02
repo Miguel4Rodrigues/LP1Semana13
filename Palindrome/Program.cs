755e274dc9ba15a2b35cb50337a3b26a85a65b84
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-06-02 15:51:19 +0100

    Add Palindrome

---
+++ Palindrome/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Palindrome
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP!");
+        }
+    }
+}
