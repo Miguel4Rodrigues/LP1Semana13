8db0d2b69238c91c065bd9b0e56580725e8fecfa
Miguel Rodrigues <miguel4rodrigues@gmail.com>
2026-06-02 18:53:14 +0100

    Update SuperList new methods

--- MultipleStuff/SuperList.cs
+++ MultipleStuff/SuperList.cs
@@ -6,6 +6,27 @@ namespace MultipleStuff
 {
     public class SuperList : List<double>
     {
-        
+        public void GetMinMax1(out double min, out double max)
+        {
+            min = this.Min();
+            max = this.Max();
+        }
+        public struct MinMaxResult
+        {
+            public double Min { get; set; }
+            public double Max { get; set; }
+        }
+        public MinMaxResult GetMinMax2()
+        {
+            return new MinMaxResult { Min = this.Min(), Max = this.Max()};
+        }
+        public Tuple<double, double> GetMinMax3()
+        {
+            return new Tuple<double, double>(this.Min(), this.Max());
+        }
+        public (double min, double max) GetMinMax4()
+        {
+            return (this.Min(), this.Max());
+        }
     }
 }
